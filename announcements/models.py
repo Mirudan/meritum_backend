c1100b5f3e64bc5be135306b3df4cb06a9ddd50e
Alexei Voropaev <voropaev.alexei92@gmail.com>
2023-10-09 15:54:38 +0400

    ADD: model of announcements

--- announcements/models.py
+++ announcements/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class News(models.Model):
+    news_id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=256)
+    content = models.TextField()
+    image = models.BinaryField(blank=True, null=True)
+    publication_date = models.DateField()
+
+    class Meta:
+        managed = False
+        db_table = 'news'
+
+
+class Course(models.Model):
+    course_id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=256)
+    content = models.TextField()
+    image = models.BinaryField(blank=True, null=True)
+    publication_date = models.DateField()
+    teacher = models.ForeignKey('Teacher', models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'course'
