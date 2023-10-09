210057e4bd8d0b2077831ef37add704eb4a8ee53
Alexei Voropaev <voropaev.alexei92@gmail.com>
2023-10-09 16:27:45 +0400

    ADD: model of groups

--- groups/models.py
+++ groups/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class Specialization(models.Model):
+    specialization_id = models.AutoField(primary_key=True)
+    name = models.TextField()
+
+
+class Semester(models.Model):
+    semester_id = models.AutoField(primary_key=True)
+    number = models.CharField(max_length=64)
+    specialization = models.ForeignKey('Specialization', models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'semester'
+
+
+class Class(models.Model):
+    class_id = models.AutoField(primary_key=True)
+    number = models.IntegerField()
+    semester = models.ForeignKey('Semester', models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'class'
