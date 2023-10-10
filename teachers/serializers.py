8662c874e0e45f73602f47da9ef818a6700e1488
Alexei Voropaev <voropaev.alexei92@gmail.com>
2023-10-10 12:20:24 +0400

    ADD: serializers of admins

--- teachers/serializers.py
+++ teachers/serializers.py
@@ -3,7 +3,7 @@ from rest_framework import serializers
 from teachers.models import Teacher
 
 
-class SpecializationSerializer(serializers.ModelSerializer):
+class TeacherSerializer(serializers.ModelSerializer):
     class Meta:
         model = Teacher
         fields = '__all__'
