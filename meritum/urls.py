a9591431daac9357491f5a02ce9fdddc684ac2be
Alexei Voropaev <voropaev.alexei92@gmail.com>
2023-10-16 22:14:23 +0400

    ADD: News API

--- meritum/urls.py
+++ meritum/urls.py
@@ -4,6 +4,8 @@ from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework import permissions
 
+from announcements.views import NewsAPIList, NewsAPIDetails
+
 schema_view = get_schema_view(
     openapi.Info(
         title="Meritum API",
@@ -20,4 +22,6 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('api/v1/newslist/', NewsAPIList.as_view()),
+    path('api/v1/newslist/<int:pk>/', NewsAPIDetails.as_view({'get': 'retrieve'})),
 ]
