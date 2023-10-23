a641db5e6696f3949a64bb3b1bb27eeae8e64f4f
Alexei Voropaev <voropaev.alexei92@gmail.com>
2023-10-23 13:51:15 +0400

    ADD: views for courses

--- announcements/views.py
+++ announcements/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from rest_framework import generics, viewsets
 from rest_framework.response import Response
 
-from announcements.models import News
-from announcements.serializers import NewsSerializer
+from announcements.models import News, Course
+from announcements.serializers import NewsSerializer, CourseSerializer
 
 
 class NewsAPIList(generics.ListAPIView):
@@ -25,4 +25,26 @@ class NewsAPIDetails(viewsets.ViewSet):
             return Response(status=404)
 
         serializer_class = NewsSerializer(news)
+        return Response(serializer_class.data)
+
+
+class CourseAPIList(generics.ListAPIView):
+    """
+    Показывает весь список объявлений о доп курсах
+    """
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+
+
+class CourseAPIDetails(viewsets.ViewSet):
+    def retrieve(self, request, pk=None):
+        """
+        Выводит одно сообщение о доп курсах по индексу
+        """
+        try:
+            course = Course.objects.get(pk=pk)
+        except Course.DoesNotExist:
+            return Response(status=404)
+
+        serializer_class = CourseSerializer(course)
         return Response(serializer_class.data)
