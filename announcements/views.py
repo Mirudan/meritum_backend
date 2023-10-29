487048e574c2bc7d8bc4d97319fca89446f76401
Alexei Voropaev <chaos.73@yandex.ru>
2023-10-29 21:30:22 +0400

    CHANGE: optimization views Courses & News

--- announcements/views.py
+++ announcements/views.py
@@ -14,18 +14,12 @@ class NewsAPIList(generics.ListAPIView):
     serializer_class = NewsSerializer
 
 
-class NewsAPIDetails(viewsets.ViewSet):
-    def retrieve(self, request, pk=None):
-        """
-        Выводит одну новость по индексу
-        """
-        try:
-            news = News.objects.get(pk=pk)
-        except News.DoesNotExist:
-            return Response(status=404)
-
-        serializer_class = NewsSerializer(news)
-        return Response(serializer_class.data)
+class NewsAPIDetails(generics.RetrieveAPIView):
+    """
+    Выводит одну новость по индексу
+    """
+    queryset = News.objects.all()
+    serializer_class = NewsSerializer
 
 
 class CourseAPIList(generics.ListAPIView):
@@ -36,15 +30,9 @@ class CourseAPIList(generics.ListAPIView):
     serializer_class = CourseSerializer
 
 
-class CourseAPIDetails(viewsets.ViewSet):
-    def retrieve(self, request, pk=None):
-        """
-        Выводит одно сообщение о доп курсах по индексу
-        """
-        try:
-            course = Course.objects.get(pk=pk)
-        except Course.DoesNotExist:
-            return Response(status=404)
-
-        serializer_class = CourseSerializer(course)
-        return Response(serializer_class.data)
+class CourseAPIDetails(generics.RetrieveAPIView):
+    """
+    Выводит одно сообщение о доп курсах по индексу
+    """
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
