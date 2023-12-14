48f980f952c675c0888a3eecbaf936fe0a7f7185
Alexei Voropaev <voropaev.alexei92@gmail.com>
2023-12-14 16:33:37 +0400

    Изменения путей к новостям и дневнику

--- meritum/urls.py
+++ meritum/urls.py
@@ -6,7 +6,6 @@ from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework import permissions
 
-
 schema_view = get_schema_view(
     openapi.Info(
         title="Meritum API",
@@ -20,16 +19,14 @@ schema_view = get_schema_view(
     permission_classes=(permissions.AllowAny,),
 )
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    # получение списка API адресов
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    # пути к новостям и курсам
-    path('api/v1/', include('announcements.urls')),
-    # отображение данных для управления расписанием
-    path('api/v1/schedule/', include('diary.urls')),
-    path('api/v1/admins/', include('admins.urls')),
-    path('api/v1/students/', include('students.urls')),
-    path('api/v1/teachers/', include('teachers.urls')),
-    path('api/v1/', include('diary.urls'))
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns = [path('admin/', admin.site.urls),
+               # получение списка API адресов
+               path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+               # пути к новостям и курсам
+               path('api/v1/announcements/', include('announcements.urls')),
+               # отображение данных для управления расписанием
+               path('api/v1/diary/', include('diary.urls')),
+               path('api/v1/admins/', include('admins.urls')),
+               path('api/v1/students/', include('students.urls')),
+               path('api/v1/teachers/', include('teachers.urls')),
+               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
