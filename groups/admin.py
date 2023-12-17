74794b7c3f59dc26bc77f5f573a41d0745266335
Alexei Voropaev <voropaev.alexei92@gmail.com>
2023-12-17 19:28:20 +0400

    правка моделей группы

--- groups/admin.py
+++ groups/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from groups.models import Specialization, Semester, ClassField
+from groups.models import Specialization, ClassField
 
 
 @admin.register(Specialization)
@@ -10,15 +10,15 @@ class SpecializationAdmin(admin.ModelAdmin):
     search_fields = ('specialization_id', 'name')
 
 
-@admin.register(Semester)
-class SemesterAdmin(admin.ModelAdmin):
-    list_display = ('number', 'specialization')
-    list_filter = ('number', 'specialization')
-    search_fields = ('number', 'specialization')
+# @admin.register(Semester)
+# class SemesterAdmin(admin.ModelAdmin):
+#     list_display = ('number', 'specialization')
+#     list_filter = ('number', 'specialization')
+#     search_fields = ('number', 'specialization')
 
 
 @admin.register(ClassField)
 class ClassFieldAdmin(admin.ModelAdmin):
-    list_display = ('number', 'semester')
-    list_filter = ('number', 'semester')
-    search_fields = ('number', 'semester')
+    list_display = ('number', )
+    list_filter = ('number', )
+    search_fields = ('number', )
