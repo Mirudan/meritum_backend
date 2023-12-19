b2faf69c560c5798611d5585ed17f156828ad1ce
Alexei Voropaev <voropaev.alexei92@gmail.com>
2023-12-19 13:56:18 +0400

    fix

--- groups/serializers.py
+++ groups/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from groups.models import Specialization, ClassField
+from groups.models import Specialization, Semester, ClassField
 
 
 class SpecializationSerializer(serializers.ModelSerializer):
@@ -9,7 +9,17 @@ class SpecializationSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
+class SemesterSerializer(serializers.ModelSerializer):
+    specialization = SpecializationSerializer()
+
+    class Meta:
+        model = Semester
+        fields = ['semester_id', 'number', 'specialization']
+
+
 class ClassFieldSerializer(serializers.ModelSerializer):
+    semester = SemesterSerializer()
+
     class Meta:
         model = ClassField
-        fields = ['class_field_id', 'number']
+        fields = ['class_field_id', 'number', 'semester']
