13699fa4c289316e4e12600bc9913dacf6180348
Alexei Voropaev <voropaev.alexei92@gmail.com>
2023-12-19 13:51:23 +0400

    delete semester

--- groups/models.py
+++ groups/models.py
@@ -16,26 +16,25 @@ class Specialization(models.Model):
         ordering = ['specialization_id']
 
 
-class Semester(models.Model):
-    semester_id = models.AutoField(primary_key=True)
-    number = models.CharField(max_length=64)
-    specialization = models.ForeignKey(Specialization, models.DO_NOTHING)
-
-    def __str__(self):
-        return str(self.semester_id)
-
-    class Meta:
-        # managed = False
-        # db_table = 'semester'
-        verbose_name = 'semester'
-        verbose_name_plural = 'semesters'
-        ordering = ['semester_id']
+# class Semester(models.Model):
+#     semester_id = models.AutoField(primary_key=True)
+#     number = models.CharField(max_length=64)
+#     specialization = models.ForeignKey(Specialization, models.DO_NOTHING)
+#
+#     def __str__(self):
+#         return str(self.semester_id)
+#
+#     class Meta:
+#         # managed = False
+#         # db_table = 'semester'
+#         verbose_name = 'semester'
+#         verbose_name_plural = 'semesters'
+#         ordering = ['semester_id']
 
 
 class ClassField(models.Model):
     class_field_id = models.AutoField(primary_key=True)
     number = models.IntegerField()
-    semester = models.ForeignKey(Semester, models.DO_NOTHING)
 
     def __str__(self):
         return str(self.class_field_id)
--- groups/serializers.py
+++ groups/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from groups.models import Specialization, Semester, ClassField
+from groups.models import Specialization, ClassField
 
 
 class SpecializationSerializer(serializers.ModelSerializer):
@@ -9,17 +9,7 @@ class SpecializationSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
-class SemesterSerializer(serializers.ModelSerializer):
-    specialization = SpecializationSerializer()
-
-    class Meta:
-        model = Semester
-        fields = ['semester_id', 'number', 'specialization']
-
-
 class ClassFieldSerializer(serializers.ModelSerializer):
-    semester = SemesterSerializer()
-
     class Meta:
         model = ClassField
-        fields = ['class_field_id', 'number', 'semester']
+        fields = ['class_field_id', 'number']
